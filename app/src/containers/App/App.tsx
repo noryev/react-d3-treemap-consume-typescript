ac0b91de567fcf57020ce717bdcf94144ca47781
Jose Quinto <quintorel@hotmail.com>
2017-04-17 15:24:30 +0100

    first release

---
+++ app/src/containers/App/App.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import TreeMap from "react-d3-treemap";
+import { data } from "../../data/data";
+// Be sure to include styles at some point
+import "react-d3-treemap/dist/react.d3.treemap.css";
+
+export default class App extends React.Component<{}, {}> {
+
+    public render() {
+        return (
+            <div className="app-container">
+                <TreeMap
+                    height={500}
+                    width={800}
+                    data={data}
+                    valueUnit={"MB"}
+                    // bgColorRangeLow={"#FFFFBF"}
+                    // bgColorRangeHigh={"#91CF60"}
+                />
+            </div>
+        );
+    }
+}
